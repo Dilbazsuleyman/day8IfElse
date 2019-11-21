a92f76d166737128d59653c620f69770170b2798
Dilbazsuleyman <56804853+Dilbazsuleyman@users.noreply.github.com>
2019-11-21 18:01:07 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int num=0;
+	if(num%2==0){
+        System.out.println("even");
+    }
+	else{
+        System.out.println("odd");
+    }
+        if(num==0){
+            System.out.println("zero");
+        }
+        if(num>0){
+            System.out.println("positive");
+        }
+
+        if(num<0){
+            System.out.println("negative");
+        }
+
+
+
+    }
+}
